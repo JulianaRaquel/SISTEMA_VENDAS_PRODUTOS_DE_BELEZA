15564a89eb7e327065d4dd4cc6bc3868c676bca2
Juliana Raquel <julianamedeiros_228@hotmail.com>
2022-08-07 16:16:50 -0300

    22222

--- jennysbeauty/urls.py
+++ jennysbeauty/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('produto.urls')),
     path('pedido/', include('pedido.urls')),
     path('perfil/', include('usuarios.urls')),
